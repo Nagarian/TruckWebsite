bc06f34f7798eb5c681db5f71d2a3696d62ad468
Udolwa34 <ludovic.caisso@y-nov.com>
2016-01-27 22:48:51 +0100

    Ajout fonctionnement réel de la gestion des pannes

--- public/js/gestion_pannes.js
+++ public/js/gestion_pannes.js
@@ -1,25 +1,28 @@
 $(window).load(function(){
 	//Récupération de la liste des pannes
-	//FAKE : pannes
-	//pannes = [];
-	pannes = [
-		{'id': '1', 'date':'12/01/2016', 'nomMission' : 'Livraison de lait', 'description': 'description bidon d\'une mission trop cool.' },
-		{'id': '2', 'date':'15/01/2016', 'nomMission' : 'Livraison de PQ', 'description': 'description bidon d\'une mission trop cool.' },
-		{'id': '3', 'date':'16/01/2016', 'nomMission' : 'Livraison de jus d\'orange', 'description': 'description bidon d\'une mission trop cool.' }
-	];
+	API_PATH = "http://cgptruck.azurewebsites.net/";
+	pannes = [];
+	recupReparateursDone = false; //Permet de ne faire qu'une seule récupération des repairers
+	clicPostReparateur = false; //Bloque le double-post du formulaire d'envoi de repairer
 
 	//Appel de la méthode de récupération des pannes / 10 secondes
     setInterval(getPannesFromJSON, 10000);
     getPannesFromJSON();
 
+    //Gestion d'ouverture de la pop-up
+    $(".boutonsOpenPannes").click(function(){
+    	//On cache l'id de la panne en cours de traitement;
+		$("#hidFailureId").val($(this).attr('id').split("__")[1]);
+    });
+
     //Clic pour la pop-up
     $("#envoiReparateurBTN").click(function(){
 		postEnvoiReparateur();
     });
+
 });
 
 function getPannesFromJSON(){
-	API_PATH = "http://cgptruck.azurewebsites.net/";
     mytoken = "Bearer " + $("#spanToken").text();
 	$.ajax({
 	    url : API_PATH + 'api/Failures/declared',
@@ -30,14 +33,27 @@ function getPannesFromJSON(){
 	    dataType : 'html',
 	    success : function(resultat, statut){
 	    	console.log("/!\\ Récupération des pannes");
-	       	//supprimerBillets();
-	       	//FAKE : traitement du résultat => Transformation en tableau de pannes
-			//gestionBillets();
+	       	supprimerBillets();
+	       	pushDonneesPanneTableau(resolveReferences(JSON.parse(resultat)));
+			gestionBillets();
 	    },
 
 	    error : function(resultat, statut, erreur){
+	    	console.log("/!\\ Erreur lors de la récupération des pannes");
 	    }
 	});
+}
+
+function pushDonneesPanneTableau(fails){
+	for (i=0; i<fails.length; i++){
+		var dateFail = new Date(fails[i].Date);
+		pannes.push({
+			"id" : fails[i].Id,
+			"date" : dateFail.getDate() +"/"+(dateFail.getMonth()+1)+"/"+dateFail.getFullYear()+" "+dateFail.getHours()+":"+dateFail.getMinutes(),
+			"nomMission" : fails[i].Mission.Name,
+			"description" : fails[i].Mission.Description
+		});
+	}
 }
 
 //Génération des billets en fonction du nombre de pannes
@@ -48,6 +64,12 @@ function gestionBillets(){
 	if (pannes.length > 0){
 		$("#badgeNombrePanne").addClass('bg-red'); //Badge en rouge
 		
+		//Si il y a une panne et qu'on a pas encore récupéré les réparateurs, on le fait.
+		if (!recupReparateursDone){
+			recupReparateurDone = true;
+			recupListeReparateurs();
+		}
+
 		//On supprime les billets
 		supprimerBillets();
 		//On crée les billets pour chacune d'entre-elles
@@ -73,7 +95,7 @@ function creerBillet(panne){
     +            panne['description']
     +        "</span>"
     +        "<span>"
-    +             "<button type='button' style='width:100%;padding:3px;'' class='btn btn-danger btn-xs' data-toggle='modal' data-target='#pannesPopupConsultPanne'>" 
+    +             "<button id='BTNopenModalPanne__'"+panne['id']+"' type='button' style='width:100%;padding:3px;'' class='boutonsOpenPannes btn btn-danger btn-xs' data-toggle='modal' data-target='#pannesPopupConsultPanne'>" 
     +                "<i class='fa fa-car'>  </i> Envoyer un réparateur"
     +            "</button>"
     +        "</span>"
@@ -84,38 +106,74 @@ function creerBillet(panne){
 
 //Suppression de tous les billets
 function supprimerBillets(){
-	//pannes = [];
+	pannes = [];
 	$("#menuPannes li").remove();
 }
 
 
 
 /********* GESTION PANNES - POP-UP DE CONSULTATION D'UNE PANNE ********/
-function postEnvoiReparateur(){
+//Récupération de la liste des réparateurs + remplissage du select
+function recupListeReparateurs(){
+	mytoken = "Bearer " + $("#spanToken").text();
 	$.ajax({
-	    url : '/envoiReparateur',
-	    type : 'POST',
-	    dataType : 'html',
-	    data : {
-	    	reparateurId : $("#reparateurIdFormSelect option[selected]").val(),
-	    	missionId : $("#hidMissionId").val()
+	    url : API_PATH + 'api/Users/repairers',
+	    headers: {
+	        'Authorization': mytoken
 	    },
-	    success : function(code_html, statut){
-	       	supprimerBillets();
-	       	//FAKE : traitement du résultat => Transformation en tableau de pannes
-			gestionBillets();
+	    type : 'GET',
+	    success : function(resultat, statut){
+	    	repairers = resolveReferences(JSON.parse(resultat));
+	    	for (i=0; i<repairers.length; i++){
+				$("#reparateurIdFormSelect").append("<option value='"+repairers[i].Id+"'>"+ repairers[i].FirstName +" "+ repairers[i].LastName +"</option>");
+			}
 	    },
-
 	    error : function(resultat, statut, erreur){
-	    },
-
-	    complete : function(resultat, statut){
-	   		console.log("Récupération des pannes");
-	   		supprimerBillets();
-	       	//FAKE : traitement du résultat => Transformation en tableau de pannes
-			gestionBillets();
+	    	//ERREUR
 	    }
 	});
+	
+}
+
+
+//POST du formulaire d'envoi d'un réparateur sur place
+function postEnvoiReparateur(){
+	if (clicPostReparateur){
+		clicPostReparateur = false;
+
+		repairerId = $("#reparateurIdFormSelect option:selected").val();
+		failureId = $("#hidFailureId").val();
+		if (repairerId == "" || failureId == ""){
+			alert("Veuillez indiquer le réparateur à envoyer sur place avant de poursuivre");
+			clicPostReparateur = true;
+			return;
+		} 
+		var dataFailure = { "RepairerId": repairerId };
+		$.ajax({
+			url : API_PATH + 'api/Failures/'+failureId+'/assign',
+			headers: {
+				'Authorization': "Bearer " + $("#spanToken").text(),
+				'Content-Type': 'text/json'
+			},
+			type : 'POST',
+			dataType: 'json',
+			data: JSON.stringify(dataFailure),
+			success : function(resultat, statut){
+				alert("Le technicien renseigné a été prévenu.");
+			},
+			error : function(resultat, statut, erreur){
+				alert("Une erreur est survenue. Veuillez réessayer plus tard");
+			},
+			complete : function(resultat, statut){
+				//Fermeture de la pop-up => click sur "Annuler"
+				clicPostReparateur = true;
+				$("#hidFailureId").val("");
+				$("#BTNcloseModalEnvoiRepar").click();
+				getPannesFromJSON(); //On recommence la procédure de récup' des pannes
+			}
+		});
+
+	}
 }
 
 
