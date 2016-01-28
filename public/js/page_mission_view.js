14ef99146a5c124f3e97c97c05e2921dac0aaa9b
Udolwa34 <ludovic.caisso@y-nov.com>
2016-01-28 11:19:16 +0100

    Map liste missions avec decalages

--- public/js/page_mission_view.js
+++ public/js/page_mission_view.js
@@ -1,6 +1,7 @@
 $(window).load(function(){
 	//Récupération des infos de la MAP
 	locations = [];
+    positions = [];
 	buildArrayWithEtapes();
     turnLocationsIntoMarkers();
 });
@@ -9,6 +10,7 @@ $(window).load(function(){
 function buildArrayWithEtapes(){
 	$("#tableEtapes tbody tr").each(function(){
 		var position = $(this).find("td:nth-child(5)").text().split(":");
+        gestionDecalagePointsMap(position);
 		locations.push({
 	    	"name" : $(this).find("td:nth-child(3)").text(),
 	    	"num" : $(this).find("td:nth-child(1)").text(),
@@ -42,4 +44,15 @@ function turnLocationsIntoMarkers() {
     var listener = googlMap.event.addListener(map, "idle", function () {
         googlMap.event.removeListener(listener);
     });
+}
+
+function gestionDecalagePointsMap(position){
+    if (locations.length > 0){
+        for (i = 0; i < locations.length; i++){
+            if ( locations[i]["lat"] == position[0] && locations[i]["long"] == position[1] ){
+                position[0] = parseFloat(position[0]) + 0.00002;
+                position[1] = parseFloat(position[1]) + 0.00002;
+            }
+        }
+    }
 }
